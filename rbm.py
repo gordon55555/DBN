a69d6279470b10e8c1d49dd28231812a9d2b68ab
wuxupeng <765074575@qq.com>
2014-12-02 15:45:11 +0800

    this is modify

--- rbm.py
+++ rbm.py
@@ -1,7 +1,7 @@
 """This tutorial introduces restricted boltzmann machines (RBM) using Theano.
 
 Boltzmann Machines (BMs) are a particular form of energy-based model which
-contain hidden variables. Restricted Boltzmann Machines further restrict BMs
+ffcontain hidden variables. Restricted Boltzmann Machines ifurther restrict BMs
 to those without visible-visible and hidden-hidden connections.
 """
 import time
